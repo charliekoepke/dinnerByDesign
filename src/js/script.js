d989ac0eec6ae270181685bcf84dd6ddfc80babd
Charlie Koepke <Charlie.Koepke@colorado.edu>
2023-02-08 13:37:02 -0700

    added code source, changed layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
+// Source: https://codepen.io/z-/pen/MQRGOe
+
 // Change variable names
 
 console.clear();
 // Put recipes and steps into a text file and parse in
-var _data = JSON.parse(`{"lyrics":[{"line":"","time":-1},{"line":"Hey, let's all go into the forest","note":"Verse 1","time":16000},{"line":"Nobody will notice for a while","time":20000},{"line":"There we can visit all the creatures","time":24000},{"line":"Maybe they can teach us facts of life","time":27500},{"line":"","time":32000},{"line":"Or we can travel to the ocean","note":"Verse 2","time":55500},{"line":"Don't forget your lotion","time":59500},{"line":"It's quite hot","time":61500},{"line":"I once met seven lovely crabs","time":64000},{"line":"They said I should go back and join them for tea","time":67500},{"line":"","time":72000},{"line":"Oh wait, the forest got demolished","note":"Verse 3","time":95500},{"line":"When they built the airport years ago","time":99000},{"line":"But we can still go see the ocean","time":103500},{"line":"Cause they put it in a bowl at the mall","time":107500},{"line":"","time":112000}]}`);
+var _data = JSON.parse(`{"lyrics":[{"line":"","time":-1},{"line":"Test test test","time":0},{"line":"Test TEst TEst","time":10000},{"line":"Test test test test","time":20000}]}`);
 // Parse from text file
 
 var currentLine = "";
